2a8acbc4509039e14fc21b15a115446211439292
Jeff Browning <jeff@jkbrowning.com>
2012-12-16 23:19:16 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moony::Application.config.secret_token = '302a0d5c7adb578312aed05680157845e5cdcf11bd3a3b652d0f8bcebf9888d23bdcf508d8898d508a66154218607a169ee6ce2b6f53933e21281c3e6db4c0ea'
